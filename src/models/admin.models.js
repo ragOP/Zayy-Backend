bffb43d7acd25704cd5514159c5ad496b67ee86d
farish-jamal <farishjamal98@gmail.com>
2024-03-26 03:41:33 +0530

    Update in schema

--- src/models/admin.models.js
+++ src/models/admin.models.js
@@ -4,12 +4,10 @@ const adminSchema = mongoose.Schema(
   {
     email: {
       type: String,
-      required: true,
       unique: true,
     },
     password: {
       type: String,
-      required: true,
     },
   },
   { timestamps: true }
--- src/models/seller.models.js
+++ src/models/seller.models.js
@@ -3,20 +3,16 @@ const mongoose = require("mongoose");
 const sellerSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
     unique: true,
   },
   password: {
     type: String,
-    required: true,
   },
   name: {
     type: String,
-    required: true,
   },
   logo: {
     type: String,
-    required: true,
   },
   website: {
     type: String,
@@ -25,27 +21,21 @@ const sellerSchema = new mongoose.Schema({
     {
       pincode: {
         type: Number,
-        required: true,
       },
       address: {
         type: String,
-        required: true,
       },
       localty: {
         type: String,
-        required: true,
       },
       city: {
         type: String,
-        required: true,
       },
       state: {
         type: String,
-        required: true,
       },
       country: {
         type: String,
-        required: true,
       },
     },
   ],
@@ -53,30 +43,24 @@ const sellerSchema = new mongoose.Schema({
     {
       account_no: {
         type: String,
-        required: true,
       },
       ifsc_code: {
         type: String,
-        required: true,
       },
       bank_name: {
         type: String,
-        required: true,
       },
       branch_name: {
         type: String,
-        required: true,
       },
       account_holder_name: {
         type: String,
-        required: true,
       },
     },
   ],
   business_type: {
     type: String,
     enum: ["boutique", "brand"],
-    required: true,
   },
 });
 
